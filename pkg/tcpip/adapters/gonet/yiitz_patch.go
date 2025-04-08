ad25b65eb1984136e2201c8cffbd1267ed0c80fc
yiitz <fufulove2012@gmail.com>
2025-04-08 17:24:28 +0800

    refine udp

--- pkg/tcpip/adapters/gonet/yiitz_patch.go
+++ pkg/tcpip/adapters/gonet/yiitz_patch.go
@@ -1,31 +1,63 @@
 package gonet
 
 import (
+	"errors"
+	"io"
+	"net"
+
 	"gvisor.dev/gvisor/pkg/tcpip"
 	"gvisor.dev/gvisor/pkg/waiter"
 )
 
-type UDPReadChan struct {
-	Ep        tcpip.Endpoint
-	WaitEntry waiter.Entry
-	NotifyCh  chan struct{}
-	DealineCh <-chan struct{}
-}
+func (c *UDPConn) ReadTo(w io.Writer) (int, net.Addr, error) {
+	deadline := c.readCancel()
 
-func (c *UDPConn) BeginReadChan() *UDPReadChan {
-	waitEntry, notifyCh := waiter.NewChannelEntry(waiter.ReadableEvents)
-	c.wq.EventRegister(&waitEntry)
-	return &UDPReadChan{
-		WaitEntry: waitEntry,
-		NotifyCh:  notifyCh,
-		DealineCh: c.readCancel(),
+	var addr tcpip.FullAddress
+	n, err := commonReadTo(w, c.ep, c.wq, deadline, &addr, c)
+	if err != nil {
+		return 0, nil, err
 	}
+	return n, fullToUDPAddr(addr), nil
 }
 
-func (c *UDPConn) EndReadChan(rc *UDPReadChan) {
-	c.wq.EventUnregister(&rc.WaitEntry)
-}
+func commonReadTo(w io.Writer, ep tcpip.Endpoint, wq *waiter.Queue, deadline <-chan struct{}, addr *tcpip.FullAddress, errorer opErrorer) (int, error) {
+	select {
+	case <-deadline:
+		return 0, errorer.newOpError("read", &timeoutError{})
+	default:
+	}
 
-func (c *UDPConn) EP() tcpip.Endpoint {
-	return c.ep
+	opts := tcpip.ReadOptions{NeedRemoteAddr: addr != nil}
+	res, err := ep.Read(w, opts)
+
+	if _, ok := err.(*tcpip.ErrWouldBlock); ok {
+		// Create wait queue entry that notifies a channel.
+		waitEntry, notifyCh := waiter.NewChannelEntry(waiter.ReadableEvents)
+		wq.EventRegister(&waitEntry)
+		defer wq.EventUnregister(&waitEntry)
+		for {
+			res, err = ep.Read(w, opts)
+			if _, ok := err.(*tcpip.ErrWouldBlock); !ok {
+				break
+			}
+			select {
+			case <-deadline:
+				return 0, errorer.newOpError("read", &timeoutError{})
+			case <-notifyCh:
+			}
+		}
+	}
+
+	if _, ok := err.(*tcpip.ErrClosedForReceive); ok {
+		return 0, io.EOF
+	}
+
+	if err != nil {
+		return 0, errorer.newOpError("read", errors.New(err.String()))
+	}
+
+	if addr != nil {
+		*addr = res.RemoteAddr
+	}
+	return res.Count, nil
 }
